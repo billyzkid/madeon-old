491de716f71dcd2bf61d0dcf1d40b7029bb321e8
Will Allan <billyzkid@yahoo.com>
2016-10-06 16:14:34 -0700

    Reorganized file structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <epic-lols>
+        <div className="main-bg active"></div>
+        <album-art></album-art>
+        <madeon-ui></madeon-ui>
+      </epic-lols>
+    );
+  }
+}
+
+export default App;
