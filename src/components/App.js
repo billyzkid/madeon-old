91d176f3a77a64b0a21dff38c8d2163782d75e7f
Will Allan <billyzkid@yahoo.com>
2016-10-06 20:21:40 -0700

    Added support for multiple app instances

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
+import classnames from "classnames";
 import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      touchSupport: false,
+      loading: true,
+      bgLoaded: false,
+      animate: false,
+      isLooperActive: false,
+      hasError: false
+    };
+  }
+
+  onMainBgClick(e) {
+    this.setState({ bgLoaded: true });
+  }
+
   render() {
+    let appClassNames = classnames({
+      "app": true,
+      "no-touch": !this.state.touchSupport,
+      "loading": this.state.loading,
+      "bgLoaded": this.state.bgLoaded,
+      "animate": this.state.animate,
+      "looper-active": this.state.isLooperActive,
+      "error": this.state.hasError
+    });
+
+    let mainBgClassNames = classnames({
+      "main-bg": true,
+      "active": this.state.bgLoaded
+    });
+
     return (
-      <epic-lols>
-        <div className="main-bg active"></div>
-        <album-art></album-art>
-        <madeon-ui></madeon-ui>
-      </epic-lols>
+      <div className={appClassNames}>
+        <div className={mainBgClassNames} onClick={this.onMainBgClick.bind(this)}></div>
+        <div className="album-art"></div>
+        <div className="madeon-ui"></div>
+      </div>
     );
   }
 }
