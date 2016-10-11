d2ab7b73d53a4e7620ab98866d8d00151d7e7dcc
Will Allan <billyzkid@yahoo.com>
2016-10-10 18:14:56 -0700

    Added components

---
+++ src/components/Chevron.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Chevron.css";
+
+class Chevron extends React.Component {
+  render() {
+    return (
+      <div className="chevron">
+        <div className="chevron-top"/>
+        <div className="chevron-bottom"/>
+      </div>
+    );
+  }
+}
+
+export default Chevron;
--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,10 +1,55 @@
 import React from "react";
+import Chevron from "./Chevron";
+import Button from "./Button";
 import "./Interface.css";
 
 class Interface extends React.Component {
   render() {
     return (
-      <div className="interface"></div>
+      <div className="interface">
+        <div className="chevrons">
+          <Chevron/>
+          <Chevron/>
+        </div>
+        <div className="buttons">
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+          <Button/>
+        </div>
+      </div>
     );
   }
 }
