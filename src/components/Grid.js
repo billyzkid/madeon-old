ba728912fc4360142e3d0f75d0ecaa3311919b43
Will Allan <billyzkid@yahoo.com>
2016-10-21 12:01:01 -0700

    Grid stuff

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,48 +1,51 @@
 import React from "react";
-import GridButton from "./GridButton";
-import { AudioType } from "../scripts/enums";
 import "./Grid.css";
 
 class Grid extends React.Component {
   render() {
+    // <div className="grid-button sound" />
+    // <div className="grid-button sound active" />
+    // <div className="grid-button sound inactive" />
+    // <div className="grid-button sound waiting" />
+
     return (
       <div className="grid" onClick={this.props.onPlay}>
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.bass} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.drum} />
-        <GridButton audioType={AudioType.sound} />
-        <GridButton audioType={AudioType.sound} />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
+        <div className="grid-button bass" />
+        <div className="grid-button bass" />
+        <div className="grid-button bass" />
+        <div className="grid-button bass" />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
+        <div className="grid-button bass" />
+        <div className="grid-button bass" />
+        <div className="grid-button bass" />
+        <div className="grid-button drum" />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
+        <div className="grid-button bass" />
+        <div className="grid-button bass" />
+        <div className="grid-button drum" />
+        <div className="grid-button drum" />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
+        <div className="grid-button bass" />
+        <div className="grid-button drum" />
+        <div className="grid-button drum" />
+        <div className="grid-button drum" />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
+        <div className="grid-button drum" />
+        <div className="grid-button drum" />
+        <div className="grid-button drum" />
+        <div className="grid-button drum" />
+        <div className="grid-button sound" />
+        <div className="grid-button sound" />
       </div>
     );
   }
--- src/components/GridButton.test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GridButton from "./GridButton";
-
-it("renders without crashing", () => {
-  ReactDOM.render(<GridButton />, document.createElement("div"));
-});
